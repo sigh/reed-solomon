8fe7c3c9c678934dd05010ce717c8b4313f71c31
sigh <sigh.projects@gmail.com>
2020-09-06 17:13:50 +1000

    Implement decoder.

--- page.js
+++ page.js
@@ -13,9 +13,18 @@ const toHexString = (n) => {
 class Display {
   constructor() {
     this._elements = {
-      utf8in: document.getElementById('message-utf8'),
-      polyin: document.getElementById('message-poly'),
+      utf8In: document.getElementById('message-utf8'),
+      polyIn: document.getElementById('message-poly'),
       encoded: document.getElementById('message-encoded'),
+      recieved: document.getElementById('recieved-encoded'),
+      polyRec: document.getElementById('recieved-poly'),
+      syndromes: document.getElementById('syndromes'),
+      errorLocator: document.getElementById('error-locator'),
+      positions: document.getElementById('error-positions'),
+      correctionPoly: document.getElementById('correction-poly'),
+      decodedPoly: document.getElementById('decoded-poly'),
+      decodedUtf8: document.getElementById('decoded-utf8'),
+      decodedMessage: document.getElementById('decoded-message'),
     };
   }
 
@@ -29,13 +38,16 @@ class Display {
     return elem;
   }
 
+  _displayList(element, list) {
+    element.textContent = list.join(', ');
+  }
+
   _displayPolynomial(element, poly) {
     element.innerHTML = '';
     let degree = poly.length - 1;
     for (let i = 0; i < poly.length; i++) {
       element.appendChild(document.createTextNode(toHexString(poly[i])));
       let exp = degree - i;
-      console.log(exp);
       if (exp > 0) {
         element.appendChild(this._makeTextElem('var', 'x'));
       }
@@ -51,12 +63,42 @@ class Display {
   updateMessage(msg) {
     let rs = new ReedSolomon(10);
 
-    let msgUtf8 = (new TextEncoder()).encode(msg);
-    this._displayBytes(this._elements.utf8in, msgUtf8);
+    // Encoding phase.
 
-    this._displayPolynomial(this._elements.polyin, msgUtf8);
+    let msgUtf8 = (new TextEncoder()).encode(msg);
+    this._displayBytes(this._elements.utf8In, msgUtf8);
+
+    this._displayPolynomial(this._elements.polyIn, msgUtf8);
 
     let encoded = rs.encode(msgUtf8);
     this._displayBytes(this._elements.encoded, encoded);
+
+    // Decoding phase.
+    let recieved = encoded;
+    // TODO: Make this configurable.
+    recieved[1] = 0;
+    recieved[4] = 3;
+
+    this._displayBytes(this._elements.recieved, recieved);
+    this._displayPolynomial(this._elements.polyRec, recieved);
+
+    let syndromes = rs.syndromes(recieved);
+    this._displayPolynomial(this._elements.syndromes, syndromes);
+
+    let errLoc = rs.errorLocator(syndromes);
+    this._displayPolynomial(this._elements.errorLocator, errLoc);
+
+    let positions = rs.errorPositions(errLoc);
+    this._displayList(this._elements.positions, positions);
+
+    let correction = rs.errorCorrection(recieved, syndromes, errLoc, positions);
+    this._displayPolynomial(this._elements.correctionPoly, correction);
+
+    let decoded = rs.applyCorrection(recieved, correction);
+    this._displayPolynomial(this._elements.decodedPoly, decoded);
+    this._displayBytes(this._elements.decodedUtf8, decoded);
+
+    let decodedMessage = (new TextDecoder()).decode(decoded);
+    this._elements.decodedMessage.textContent = decodedMessage;
   }
 }
--- reed_solomon.js
+++ reed_solomon.js
@@ -4,6 +4,7 @@ class GF2_8 {
   static PRIM = 0x11d;
   static CHARACTERISTIC = 0x100;
   static SIZE = 0xFF;
+  static GENERATOR = 0x02;
 
   // Addition and subtraction (same).
 
@@ -39,7 +40,7 @@ class GF2_8 {
     for (let i = 0, x = 1; i < this.SIZE; i++) {
       exp[i] = x;
       x <<= 1;
-      if (x & this.CHARACTERISTIC) x ^= this.PRIM;
+      if (x & this.CHARACTERISTIC) x = this.sub(x, this.PRIM);
     }
 
     return exp;
@@ -54,6 +55,14 @@ class GF2_8 {
   })();
 
   // Polynomial functions.
+
+  static polyEval(p, x) {
+    let y = p[0];
+    for (let i = 1; i < p.length; i++) {
+      y = this.add(this.mul(y, x), p[i]);
+    }
+    return y;
+  }
 
   static polyScale(p, x) {
     return p.map(a => this.mul(a, x));
@@ -99,13 +108,13 @@ class GF2_8 {
 class ReedSolomon {
   constructor(nsym) {
     this._nsym = nsym;
-    this._gen = this._generatorPoly(nsym);
+    this._gen = this.generatorPoly(nsym);
   }
 
-  _generatorPoly(nsym) {
+  generatorPoly(nsym) {
     let g = [1];
     for (let i = 0; i < nsym; i++) {
-      g = GF2_8.polyMul(g, [1, GF2_8.pow(2, i)]);
+      g = GF2_8.polyMul(g, [1, GF2_8.EXP[i]]);
     }
     return g;
   };
@@ -121,5 +130,101 @@ class ReedSolomon {
     let [p, q] = GF2_8.polyDiv(dividend, this._gen);
 
     return new Uint8Array([...msg, ...q]);
+  }
+
+  SYND_SHIFT = 1;
+
+  syndromes(msg) {
+    let synd = new Uint8Array(this._nsym + this.SYND_SHIFT);
+    for (let i = 0; i < this._nsym; i++) {
+      synd[i+this.SYND_SHIFT] = GF2_8.polyEval(msg, GF2_8.EXP[i])
+    }
+    return synd;
+  }
+
+  errorLocator(synd) {
+    let errLoc = [1];
+    let oldLoc = [1];
+
+    for (let i = 0; i < this._nsym; i++) {
+      const k = i + this.SYND_SHIFT;
+      let delta = synd[k];
+      // TODO: Replace with polyMulAt(reverse(errLoc), synd, k)
+      for (let j = 1; j < errLoc.length; j++) {
+        delta = GF2_8.add(
+          delta,
+          GF2_8.mul(errLoc[errLoc.length-j-1], synd[k-j]));
+      }
+
+      // Multiply by x.
+      oldLoc = [...oldLoc, 0];
+
+      if (delta !== 0) {
+        if (oldLoc.length > errLoc.length) {
+            let newLoc = GF2_8.polyScale(oldLoc, delta)
+            oldLoc = GF2_8.polyScale(errLoc, GF2_8.div(1, delta));
+            errLoc = newLoc
+        }
+        errLoc = GF2_8.polyAdd(errLoc, GF2_8.polyScale(oldLoc, delta));
+      }
+    }
+    // Drop leading zeros.
+    while (errLoc.length && errLoc[0] == 0) errLoc.unshift();
+    return errLoc;
+  }
+
+  errorPositions(errLoc) {
+    let positions = [];
+    for (let i = 0; i < GF2_8.SIZE; i++) {
+      if (GF2_8.polyEval(errLoc, GF2_8.EXP[i]) == 0) {
+        positions.push(GF2_8.SIZE - i);
+      }
+    }
+    return positions;
+  }
+
+  errorEvaluator(synd, errLoc) {
+    let revSynd = [...synd].reverse();
+    let mul = GF2_8.polyMul(revSynd, errLoc);
+    let result = mul.subarray(mul.length - this._nsym - 1);
+    return result;
+  }
+
+  errorCorrection(msg, synd, errLoc, errPos) {
+    let coefPos = errPos;
+    let errEval = this.errorEvaluator(synd, errLoc);
+
+    let x = [];
+    for (const pos of coefPos) {
+      let l = GF2_8.SIZE - pos;
+      x.push(GF2_8.div(1, GF2_8.EXP[l]));
+    }
+
+    // TODO: Don't need msg.
+    let e = new Uint8Array(msg.length);
+    for (let i = 0; i < x.length; i++) {
+      let errLocPrimeTmp = []
+      for (let j = 0; j < x.length; j++) {
+        if (j !== i) {
+          errLocPrimeTmp.push(GF2_8.sub(1, GF2_8.div(x[j], x[i])));
+        }
+      }
+
+      let errLocPrime = errLocPrimeTmp.reduce(GF2_8.mul.bind(GF2_8), 1);
+
+      let y = GF2_8.polyEval(errEval, GF2_8.div(1, x[i]));
+      y = GF2_8.mul(x[i], y)
+
+      let magnitude = GF2_8.div(y, errLocPrime);
+      e[msg.length-errPos[i]-1] = magnitude;
+    }
+
+    return e;
+  }
+
+  applyCorrection(msg, correction) {
+    let corrected = GF2_8.polyAdd(msg, correction);
+    let truncated = corrected.subarray(0, msg.length - this._nsym);
+    return truncated;
   }
 }
